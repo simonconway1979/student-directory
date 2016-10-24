0b51e271052dbec4758c0d588183c0c5b25e4903
Simon Conway <simon.conway@condaal.com>
2016-10-24 16:38:40 +0100

    Changed the student names to hashes so we can reference name and cohort in the code

--- directory.rb
+++ directory.rb
@@ -2,17 +2,17 @@
 #First we print the list of students (as an array)
 
 students = [
-"Dr. Hannibal Lecter",
-"Darth Vader",
-"Nurse Ratched",
-"Michael Corleone",
-"Alex DeLarge",
-"The Wicked Witch of the West",
-"Terminator",
-"Freddy Krueger",
-"The Joker",
-"Joffrey Baratheon",
-"Norman Bates"
+{name: "Dr. Hannibal Lecter", cohort: :November},
+{name: "Darth Vader", cohort: :November},
+{name: "Nurse Ratched", cohort: :November},
+{name: "Michael Corleone", cohort: :November},
+{name: "Alex DeLarge", cohort: :November},
+{name: "The Wicked Witch of the West", cohort: :November},
+{name: "Terminator", cohort: :November},
+{name: "Freddy Krueger", cohort: :November},
+{name: "The Joker", cohort: :November},
+{name: "Joffrey Baratheon", cohort: :November},
+{name: "Norman Bates", cohort: :November}
 ]
 
 def print_header
@@ -21,13 +21,13 @@ puts "--------------"
 end
 
 
-def print(names)
-names.each {|name| puts name}
+def print(students)
+students.each {|student| puts "#{student[:name]} (#{student[:cohort]} cohort)"}
 end
 
 
-def print_footer(names)
-puts "Overall we have #{names.count} great students"
+def print_footer(students)
+puts "Overall, we have #{students.count} great students"
 end
 
 print_header
