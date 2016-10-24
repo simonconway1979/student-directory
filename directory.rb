4e3468066f5e3b7ff534bb9f132176c74a623ffb
Simon Conway <simon.conway@condaal.com>
2016-10-24 16:01:55 +0100

    Refactored the student count

--- directory.rb
+++ directory.rb
@@ -1,3 +1,6 @@
+student_count = 11
+
+#First we print the list of students
 puts "The students of Villains Academy"
 puts "--------------"
 puts "Dr. Hannibal Lecter"
@@ -12,7 +15,9 @@ puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
 
+#Finally we print the number of students
 print "Overall we have "
-print 11
-print " great students"
+#Print doesn't add new line characters. 
+print student_count
+print " great students\n"
 
