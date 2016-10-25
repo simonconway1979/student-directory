25b3008f0e735adda96cc58d50c9baaff5143601
Simon Conway <simon.conway@condaal.com>
2016-10-25 04:55:49 +0100

    Refactored the interactive menu

--- directory.rb
+++ directory.rb
@@ -1,91 +1,81 @@
 
+@students = [] #An empty array accessible by all methods
+
 #First we print the list of students (as an array)
 
 
-
 def interactive_menu
-  students = []
   loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+  def print_menu
     # 1. print the menu and ask the user what to do
     puts "1. Input the students"
     puts "2. Show the students"
     puts "9. Exit" 
+  end
     
     # 2. read the input and save it into a variable
     selection = gets.chomp
+
+    def print_header
+    puts "The students of Villains Academy"
+    puts "--------------"
+    end
+
+
+    def print_student_list
+    @students.each {|student| puts "#{student[:name]} (#{student[:cohort]} cohort)"}
+    end
+
+
+    def print_footer
+      puts "Overall, we have #{@students.count} great students"
+    end
     
     
     # 3. do what the user has asked
+  def process(selection)
     case selection
     when "1"
-      # Input the students
-      student = input_students
+     input_students
     when "2"
-      # show the students
-      print_header
-      print(students)
-      print_footer(students)
-      
+      show_students
     when "9"  
       exit # this will cause the program to terminate
     else
       puts "I don't know what you meant. Try again."
-    
     end
   end
-end
+
+
+
+    def input_students
+      puts "Please enter the names of the students"
+      puts "To finish, just hit return twice"
+      name = gets.chomp
+      while !name.empty? do
+        @students << {name: name, cohort: :November}
+        puts "Now we have #{@students.count} students"
+        name = gets.chomp
+      end
+    end
+    
+    def show_students
+      # show the students
+      print_header
+      print_student_list
+      print_footer
+    end
+
+
+    interactive_menu
 
 
 
 
 
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  students = []
-  name = gets.chomp
-  while !name.empty? do
-    students << {name: name, cohort: :November}
-    puts "Now we have #{students.count} students"
-    name = gets.chomp
-  end
-  students
-end
 
-
-
-
-#students = [
-#{name: "Dr. Hannibal Lecter", cohort: :November},
-#{name: "Darth Vader", cohort: :November},
-#{name: "Nurse Ratched", cohort: :November},
-#{name: "Michael Corleone", cohort: :November},
-#{name: "Alex DeLarge", cohort: :November},
-#{name: "The Wicked Witch of the West", cohort: :November},
-#{name: "Terminator", cohort: :November},
-#{name: "Freddy Krueger", cohort: :November},
-#{name: "The Joker", cohort: :November},
-#{name: "Joffrey Baratheon", cohort: :November},
-#{name: "Norman Bates", cohort: :November}
-#]
-
-def print_header
-puts "The students of Villains Academy"
-puts "--------------"
-end
-
-
-def print(students)
-students.each_with_index {|student, index| puts "#{index + 1} #{student[:name]} (#{student[:cohort]} cohort)"}
-end
-
-
-def print_footer(students)
-puts "Overall, we have #{students.count} great students"
-end
-
-
-interactive_menu
-print_header
-print(students)
-print_footer(students)
